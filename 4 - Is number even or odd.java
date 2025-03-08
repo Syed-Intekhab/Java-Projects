3ac193151f382927f9469cd17c7a44e26b95acda
Syed Intekhab <syedintekhab.dev@gmail.com>
2025-03-08 11:02:44 +0530

    Update 4 - Is number even or odd.java

--- 4 - Is number even or odd.java
+++ 4 - Is number even or odd.java
@@ -1,1 +1,18 @@
+import java.util.*;
 
+public class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+        while(true){
+            System.out.print("Enter any number: ");
+            int num = sc.nextInt();
+            
+            if(num % 2 == 0){
+                System.out.println("Even!");
+            } else {
+                System.out.println("Odd!");
+            }
+        }
+    }
+}
