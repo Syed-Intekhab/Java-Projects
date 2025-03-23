9a9967b0e4a6e30a97e4ea982b0a298a169cee6e
Syed Intekhab <syedintekhab.dev@gmail.com>
2025-03-23 11:05:08 +0530

    Create Star Pattern - 7.java

---
+++ Star Patterns/Star Pattern - 7.java
@@ -0,0 +1,20 @@
+public class Main{
+    public static void main(String[] args){
+        int n = 5;
+        
+        for(int i = n; i >= 1; i--){
+            for(int j = 1; j <= i; j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*
+  	1 2 3 4 5
+  	1 2 3 4
+  	1 2 3 
+   	1 2 
+  	1
+*/
